bfcb4551296d3da162e93eb8bca1c744e6ad5cef
Ivan Khvostishkov <87804596+ivan-khvostishkov@users.noreply.github.com>
2026-03-12 09:08:31 +0100

    Unified EC2 logic

--- open-aws-console.ps1
+++ open-aws-console.ps1
@@ -1,13 +1,40 @@
 $ErrorActionPreference = "Stop"
 
-# Get credentials from the AWS CLI SSO cache
-$session = aws configure export-credentials | ConvertFrom-Json
+# Detect if running on EC2 by checking IMDS availability
+try {
+    $null = Invoke-RestMethod -Method PUT -Uri "http://169.254.169.254/latest/api/token" `
+        -Headers @{"X-aws-ec2-metadata-token-ttl-seconds" = "21600"} -TimeoutSec 2
+    $isEC2 = $true
+    Write-Host "Detected EC2 environment - using instance metadata for credentials"
+} catch {
+    $isEC2 = $false
+    Write-Host "Detected local environment - using AWS CLI SSO cache for credentials"
+}
 
-$AWS_ACCESS_KEY_ID = $session.AccessKeyId
-$AWS_SECRET_ACCESS_KEY = $session.SecretAccessKey
-$AWS_SESSION_TOKEN = $session.SessionToken
+if ($isEC2) {
+    # Get credentials from EC2 instance metadata
+    $token = Invoke-RestMethod -Method PUT -Uri "http://169.254.169.254/latest/api/token" `
+        -Headers @{"X-aws-ec2-metadata-token-ttl-seconds" = "21600"}
+    
+    $roleName = Invoke-RestMethod -Uri "http://169.254.169.254/latest/meta-data/iam/security-credentials/" `
+        -Headers @{"X-aws-ec2-metadata-token" = $token}
+    
+    $creds = Invoke-RestMethod -Uri "http://169.254.169.254/latest/meta-data/iam/security-credentials/$roleName" `
+        -Headers @{"X-aws-ec2-metadata-token" = $token}
+    
+    $AWS_ACCESS_KEY_ID = $creds.AccessKeyId
+    $AWS_SECRET_ACCESS_KEY = $creds.SecretAccessKey
+    $AWS_SESSION_TOKEN = $creds.Token
+} else {
+    # Get credentials from AWS CLI SSO cache
+    $session = aws configure export-credentials | ConvertFrom-Json
+    
+    $AWS_ACCESS_KEY_ID = $session.AccessKeyId
+    $AWS_SECRET_ACCESS_KEY = $session.SecretAccessKey
+    $AWS_SESSION_TOKEN = $session.SessionToken
+}
 
-# Step 2: Get a sign-in token from AWS federation endpoint
+# Get a sign-in token from AWS federation endpoint
 $sessionJson = @{
     sessionId    = $AWS_ACCESS_KEY_ID
     sessionKey   = $AWS_SECRET_ACCESS_KEY
@@ -22,7 +49,7 @@ $sessionEncoded = [System.Web.HttpUtility]::UrlEncode($sessionJson)
 $signinTokenResponse = Invoke-RestMethod -Uri "https://signin.aws.amazon.com/federation?Action=getSigninToken&Session=$sessionEncoded"
 $signinToken = $signinTokenResponse.SigninToken
 
-# Step 3: Construct the federated AWS Console URL
+# Construct the federated AWS Console URL
 $consoleUrl = "https://console.aws.amazon.com/"
 $destination = [System.Web.HttpUtility]::UrlEncode($consoleUrl)
 $federatedUrl = "https://signin.aws.amazon.com/federation?Action=login&Issuer=Example&Destination=$destination&SigninToken=$signinToken"
